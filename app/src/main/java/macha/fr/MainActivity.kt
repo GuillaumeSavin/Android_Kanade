97277b75d5cff3aa672a1986cf47e03a0eea7d7f
Guillaume Savin <guillaume.savin@etu.unice.fr>
2020-09-22 12:16:16 +0200

    Initial commit

---
+++ app/src/main/java/macha/fr/MainActivity.kt
@@ -0,0 +1,11 @@
+package macha.fr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
